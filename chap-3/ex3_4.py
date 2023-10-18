b57e40620c55330de4fbdc3940f6281980db9da9
Danai <balla.danai@gmail.com>
2023-10-18 14:51:13 +0300

    fix serious error in calculations

--- chap-3/ex3_4.py
+++ chap-3/ex3_4.py
@@ -20,39 +20,47 @@ class Field:
 
 class FunctionEvaluation:
 
-    def __init__(self, evals: list[int], p: int) -> None:
+    def __init__(self, f_evals: list[int], p: int) -> None:
         self.field = Field(p)
-        log = int(math.log(len(evals), 2))
-        if (2**log == len(evals)):
-            self.evals = evals
+        log = int(math.log(len(f_evals), 2))
+        if (2**log == len(f_evals)):
+            self.f_evals = f_evals
             # TODO: find a better name than 'log'
             self.log = log
         else:
             raise ValueError #TODO: fix the error
             
     def slow_evaluate(self, input: list[int]) -> int:
-        n = len(self.evals)
+        lagrange_basis_evals = self.find_lagnrange_basis_evals(input)
+        # now calculate sum
+        sum = 0
+        n = len(self.f_evals)
+        for i in range(n):
+            sum = self.field.add(sum, self.field.mult(self.f_evals[i], lagrange_basis_evals[i]))
+        return sum
+    
+    def find_lagnrange_basis_evals(self, input: list[int]) -> list[int]:
+        n = len(self.f_evals)
         lagrange_basis_evals = [] # this is multilinear Langrange basis polynomials with interpolating set {0,1}^n
-        #TODO: perhaps make an array of all the basis. Not the best, since we are using more space, but more readable
         for i in range(n):
             prod = 1
             # take binary representation
             basis_elem = self.__binarize(i)
-            # calculate the rest
-            for bit in basis_elem:
-                if bit == "1":
-                    prod = self.field.mult(prod, self.evals[i])
+            # calculate the basis polynomial evaluation for this basis element
+            for j in range(self.log):
+                coordinate = basis_elem[j]
+                if coordinate == "1":
+                    prod = self.field.mult(prod, input[j])
                 else:
-                    prod = self.field.mult(prod, 1-self.evals[i])
+                    prod = self.field.mult(prod, 1-input[j])
             lagrange_basis_evals.append(prod)
-        # now calculate sum
-        sum = 0
-        for i in range(n):
-            sum = self.field.add(sum, self.field.mult(self.evals[i], lagrange_basis_evals[i]))
-        return sum
+        return lagrange_basis_evals
     
     def __binarize(self, num: int)-> str:
-        b = bin(num)
+        """
+        Binary representation of a number with leading 0's
+        """
+        b = bin(num)[2:] # binary starts with "0b" so we cut that
         # Add missing 0's and return
         return "0"*(self.log - len(b)) + b
     
--- chap-3/test.py
+++ chap-3/test.py
@@ -1,5 +1,5 @@
 import unittest
-from ex3_4 import Field
+from ex3_4 import Field, FunctionEvaluation
 
 class TestField(unittest.TestCase):
 
@@ -17,6 +17,12 @@ class TestField(unittest.TestCase):
         result = field.add(a, b)
         self.assertEqual(6, result)
 
+class TestLagrangeBasisEvals(unittest.TestCase):
+    def test_lagrange_basis_evals(self):
+        f_evals = [3, 4, 1, 2]
+        test = FunctionEvaluation(f_evals, 11)
+        lagrange_basis_evals = test.find_lagnrange_basis_evals([2,4])
+        self.assertEqual(lagrange_basis_evals, [3, 7, 5, 8])
 
 if __name__ == '__main__':
     unittest.main()
